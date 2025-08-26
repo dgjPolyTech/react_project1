e7de9e016f2e7495e8ba0ac7f2992d4f045ca7a0
dgjPolyTech <dgjpolytech@gmail.com>
2025-08-26 16:15:10 +0900

    chp03 commit

--- src/index.js
+++ src/index.js
@@ -3,13 +3,18 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Library from './chp03/Library';
+import './chp03/Book.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Library />
   </React.StrictMode>
 );
+
+// index.js 실행할 때에는, root.render의 React.StrictMode 사이 위치에 해당하는 컴포넌트 명만 바꿔주면 됨.
+// 그리고 index.js는 npm start로 실행해야 함.
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
