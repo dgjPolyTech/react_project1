039c19fb84e110d3fd96fdaf1fed837a175e4b22
dgjPolyTech <dgjpolytech@gmail.com>
2025-09-02 14:58:14 +0900

    button 페이지 작성

--- src/index.js
+++ src/index.js
@@ -3,18 +3,20 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import Library from './chp03/Library';
-import './chp03/Book.css';
+import './chp04/Button.css';// 앞으로 만든거 css 적용할 때는 여기 수정
+import ConfirmDialog from "./chp04/ConfirmDialog";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Library />
+    <ConfirmDialog />
   </React.StrictMode>
 );
 
 // index.js 실행할 때에는, root.render의 React.StrictMode 사이 위치에 해당하는 컴포넌트 명만 바꿔주면 됨.
 // 그리고 index.js는 npm start로 실행해야 함.
+
+// 혹시 npm 관련 실행 안되면,
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
